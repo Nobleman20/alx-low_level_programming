ea80e90a23ad6e13a1389cd750f7c67204170f53
Nobleman20 <emanueloluyege@gmail.com>
2022-10-24 15:35:19 -0700

    Fix 2:q

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,8 +13,5 @@ void _putchar(char);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
 
+#endif
