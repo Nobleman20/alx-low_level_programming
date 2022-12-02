40b86accf4add461bd30f81235afab193cf9c7cb
Nobleman20 <emmanueloluyege@gmail.com>
2022-12-01 18:36:44 -0800

    Fix 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,18 +1,11 @@
-/*
- * File: 1-print_binary.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
-
+#include "main.h"
 /**
- * print_binary - Prints the binary representation of a number.
- * @n: The number to be printed in binary.
+ * print_binary - prints the binary representation of a numnber
+ * @n: unsigned long, number to be represented in binary
  */
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);
-
-	_putchar((n & 1) + '0');
+	(n & 1) ? _putchar('1') : _putchar('0');
 }
